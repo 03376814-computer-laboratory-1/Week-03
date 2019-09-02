ebfcbf30ce737520111a3bceba0ebb894674b546
Reem <safefire7@gmail.com>
2019-09-02 13:59:53 +0700

    Hello Computer lab.

---
+++ CL62-03/CL62-03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL62_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Computer lab.");
+
+            Console.ReadKey();
+        }
+    }
+}
