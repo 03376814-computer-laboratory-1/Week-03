43ca4ad82178694fe660b7fbe2996b8753320c6d
ReemWSW <safefire@gmail.com>
2019-09-08 22:27:02 +0700

    first year semester 1

---
+++ Engedu/Engedu/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engedu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 255x ssemester 1, I am an Engedu freshman.");
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
