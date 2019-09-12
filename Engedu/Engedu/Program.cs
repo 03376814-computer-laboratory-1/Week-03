59fc97c7b2d7c8ecd97296f6ad9548d751785dd2
ReemWSW <safefire@gmail.com>
2019-09-12 10:09:32 +0700

    Update Program.cs

--- Engedu/Engedu/Program.cs
+++ Engedu/Engedu/Program.cs
@@ -12,7 +12,11 @@ namespace Engedu
         {
             Console.WriteLine("Year 2561 semester 1, I am an Engedu freshman.");
             Console.WriteLine("Year 2561 semester 2, I learn to program in c sharp.");
+
             Console.WriteLine("Year 2562 semester 1, I have to regrade on some course.");
+
+            Console.WriteLine("Year 2562 semester 1, I start my programmer life.");
+
 
 
 
